6cf0c7e4aa7ea557280e8dde68127af4eb609054
Cloudias Imani Pradana <danapradana30@gmail.com>
2023-07-10 15:58:10 +0700

    Add AUTH JWT API

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendApiError($validator->errors()->first(), 400);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if ($token = Auth::guard('api')->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return $this->sendApiError('Unauthorized', 403);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\APIPortoController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Auth
+Route::post('login', [AuthController::class, 'login']);
 
 Route::get('list-porto',[APIPortoController::class, 'listPorto']);
